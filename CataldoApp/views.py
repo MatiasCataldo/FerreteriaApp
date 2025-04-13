c903b0e09bad9dce63cdd31004fee61f2f29cf14
MatiasCataldo <maticataldo21@gmail.com>
2025-04-13 13:43:03 -0300

    entrega numero 3

--- CataldoApp/views.py
+++ CataldoApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from CataldoApp.forms import CreacionSocio 
 from CataldoApp.models import Socio
+from django.contrib import messages 
 
 # Create your views here.
 def home(request):
@@ -12,8 +13,13 @@ def crear_socio(request):
         formulario = CreacionSocio(request.POST)
         if formulario.is_valid():
             info = formulario.cleaned_data
-            socio = Socio(nombre=info.get('nombre'), apellido=info.get('apellido'), dni=info.get('dni'), direccion=info.get('direccion'), celular=info.get('celular'))
+            socio = Socio(nombre=info.get('nombre'), 
+                        apellido=info.get('apellido'), 
+                        dni=info.get('dni'), 
+                        direccion=info.get('direccion'),
+                        celular=info.get('celular'))
             socio.save()
+            messages.success(request, '¡Socio creado con éxito!')
             return redirect('listado_de_socios')
     else:
         formulario = CreacionSocio()    
@@ -21,9 +27,9 @@ def crear_socio(request):
     return render(request, 'home/crear_socio.html', {'formulario': formulario})
 
 def listado_de_socios(request):
-    consulta = request.GET.get("q")
-    if consulta:
-        socios = Socio.objects.filter(apellido__icontains=consulta)
+    busqueda = request.GET.get('busqueda')
+    if busqueda:
+        socios = Socio.objects.filter(apellido__icontains=busqueda)
     else:
         socios = Socio.objects.all()
     return render(request, 'home/listado_de_socios.html', {'socios': socios})
